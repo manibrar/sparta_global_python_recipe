9339929ef0f1effab729f7f4cea3833c69e98922
manibrar <manvirbrar1988@gmail.com>
2018-08-01 23:13:54 +0100

    update sources

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,10 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+package 'python'
+package 'python-pip'
 apt_update 'update_sources' do
   action :update
 end
-package 'libncurses5'
-package 'python'
-package 'python-pip'
 package 'python-flask'
+package 'libncurses5-dev'
