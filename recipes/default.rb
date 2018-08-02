7bd33e1df024b7491921e33e08892e5bb5c73cee
manibrar <manvirbrar1988@gmail.com>
2018-08-02 12:12:28 +0100

    calmDown

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
-package 'python'
-package 'python-pip'
 apt_update 'update_sources' do
   action :update
 end
+package 'python'
+package 'python-pip'
 package 'python-flask'
 package 'libncurses5-dev'
